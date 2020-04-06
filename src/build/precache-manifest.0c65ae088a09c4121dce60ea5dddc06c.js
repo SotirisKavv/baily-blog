2aaea6d5393d963f8083feb92933b1a77816859a
sotiris <sotos.kavvouras@gmail.com>
2020-04-06 17:34:29 +0300

    first commit

---
+++ src/build/precache-manifest.0c65ae088a09c4121dce60ea5dddc06c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f1af95bfc58263904e04de4c090f861",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c84f1d25df5f27d56fa",
+    "url": "/static/css/main.7ffbb850.chunk.css"
+  },
+  {
+    "revision": "e2d7fed56036f2104b0c",
+    "url": "/static/js/2.ac292a11.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ac292a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c84f1d25df5f27d56fa",
+    "url": "/static/js/main.f2811cb1.chunk.js"
+  },
+  {
+    "revision": "209d7516a220ea251298",
+    "url": "/static/js/runtime-main.cc5c5978.js"
+  }
+]);
